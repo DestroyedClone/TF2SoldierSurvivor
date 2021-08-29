5bbc526bc3cdc433a0b35977eeb1d6d29bb5c37e
DestroyedClone <destroyedclone@gmail.com>
2021-08-28 21:01:40 -0700

    lib update

--- HenryMod/HenryPlugin.cs
+++ HenryMod/HenryPlugin.cs
@@ -7,7 +7,9 @@ using System.Security;
 using System.Security.Permissions;
 
 [module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
 
 namespace HenryMod
 {
